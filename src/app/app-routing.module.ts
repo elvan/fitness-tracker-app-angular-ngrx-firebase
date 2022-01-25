baf91469525ea92bbd9cacb95b1ce5763e6229a7
Elvan H <elvanhd@gmail.com>
2022-01-25 09:48:42 +0700

    creating the app structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { CreateTrainingComponent } from './training/create-training/create-training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { PreviousTrainingComponent } from './training/previous-training/previous-training.component';
+import { TrainingComponent } from './training/training/training.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'training',
+    component: TrainingComponent,
+  },
+  {
+    path: 'current-training',
+    component: CurrentTrainingComponent,
+  },
+  {
+    path: 'create-training',
+    component: CreateTrainingComponent,
+  },
+  {
+    path: 'previous-training',
+    component: PreviousTrainingComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { MaterialModule } from './material.module';
+import { CreateTrainingComponent } from './training/create-training/create-training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { PreviousTrainingComponent } from './training/previous-training/previous-training.component';
+import { TrainingComponent } from './training/training/training.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    CreateTrainingComponent,
+    PreviousTrainingComponent,
+    WelcomeComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
