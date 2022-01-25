aaf2623a1eab9f271bb3de79e1f164775b008d2e
Elvan H <elvanhd@gmail.com>
2022-01-25 13:01:41 +0700

    adding a login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -29,6 +29,7 @@ import { TrainingComponent } from './training/training/training.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(4),
+        Validators.maxLength(64),
+      ],
+    }),
+    password: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(64),
+      ],
+    }),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit(loginForm: NgForm) {
+    console.log(loginForm.value);
+  }
 }
