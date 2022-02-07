9128c7d0736d3ece2e61de8e2f3ee7834471c2d4
Elvan H <elvanhd@gmail.com>
2022-02-07 15:08:43 +0700

    passing data to the dialog

--- src/app/training/stop-training/stop-training.component.ts
+++ src/app/training/stop-training/stop-training.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-stop-training',
   templateUrl: './stop-training.component.html',
 })
-export class StopTrainingComponent {}
+export class StopTrainingComponent {
+  constructor(
+    public dialogRef: MatDialogRef<StopTrainingComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { progress: number }
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
