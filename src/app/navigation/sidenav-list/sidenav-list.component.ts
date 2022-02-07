6625d936c584530cf0d83c2fad284a9fde4be45e
Elvan H <elvanhd@gmail.com>
2022-02-08 01:13:10 +0700

    displaying content in the sidenav conditionally

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,16 +1,36 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css'],
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSidenav = new EventEmitter<void>();
 
-  constructor() {}
+  authStatus = false;
 
-  ngOnInit(): void {}
+  authSub = new Subscription();
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authSub = this.authService.authChange.subscribe((authStatus) => {
+      this.authStatus = authStatus;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe();
+  }
 
   onCloseSidenav() {
     this.closeSidenav.emit();
