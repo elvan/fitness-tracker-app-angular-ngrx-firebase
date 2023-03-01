06e9f3d4f57665b14fd2c0c581cbec618cc1695e
Elvan Hidayat <elvanhd@gmail.com>
2023-03-01 13:26:25 +0700

    add material data table

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-past-trainings',
@@ -6,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./past-trainings.component.css'],
 })
 export class PastTrainingsComponent implements OnInit {
-  constructor() {}
+  displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
+  dataSource = new MatTableDataSource<Exercise>();
 
-  ngOnInit() {}
+  constructor(private trainingService: TrainingService) {}
+
+  ngOnInit() {
+    this.dataSource.data =
+      this.trainingService.getCompletedOrCancelledExercises();
+  }
 }
